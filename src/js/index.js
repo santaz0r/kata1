1b8c290890e24d4ddb580d909d3025485c548a35
VictorM <neuron96@mail.ru>
2024-03-10 00:43:04 +0500

    feat: add slider for mobile

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  width: 240,
+  spaceBetween: 20,
+});
